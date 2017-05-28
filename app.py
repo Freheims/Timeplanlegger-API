0cac8a670ee4c79228269842a9ffeb5062566ef9
Fredrik Heimsæter <freheims@gmail.com>
2017-05-28 12:46:44 +0200

    New api-endpoints

--- app.py
+++ app.py
@@ -11,20 +11,30 @@ webapi = Api(app)
 def index():
     return "API infopage"
 
+class AreasAPI(Resource):
+    def get(self,uni):
+        return getAreas(uni)
+
 class BuildingsAPI(Resource):
     def get(self, uni):
-        filename = uni + ".json"
-        file = open(filename, "r")
-        content = json.load(file)
-        file.close()
-        return content
-        #return getBuildings(university)
+        return getBuildingsInArea(uni, area)
+
+class RoomsAPI(Resource):
+    def get(self, uni, area, building):
+        return getRoomsInBuilding(uni, area, building)
+
+class RoomDataAPI(Resource):
+    def get(self, uni, area, building, room):
+        return getRoomData(uni, area, building, room)
 
 class ScheduleAPI(Resource):
     def get(self, uni, area, building, room, week, year):
         return api.getWeekScheduleForRoom(uni, area, building, room, week, year)
 
-webapi.add_resource(BuildingsAPI, '/api/buildings/<string:uni>', endpoint = 'buildings')
+webapi.add_resource(AreasAPI, '/api/areas/<string:uni>', endpoint = 'areas')
+webapi.add_resource(BuildingsAPI, '/api/buildings/<string:uni>/<string:area>', endpoint = 'buildings')
+webapi.add_resource(RoomsAPI, '/api/rooms/<string:uni>/<string:area>/<string:building>', endpoint = 'rooms')
+webapi.add_resource(RoomDataAPI, '/api/rooms/<string:uni>/<string:area>/<string:building>/<string:room>', endpoint = 'roomdata')
 webapi.add_resource(ScheduleAPI, '/api/schedule/<string:uni>/<string:area>/<string:building>/<string:room>/<int:week>/<int:year>', endpoint = 'schedule')
 
 
