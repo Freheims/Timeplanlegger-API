320952244893b02b33f207fba0083e78f448a9f9
Fredrik Heimsæter <freheims@gmail.com>
2017-12-24 00:57:27 +0100

    Revrite of api due to changes in TP and new needs

--- api.py
+++ api.py
@@ -12,15 +12,8 @@ def getAreas(university):
     r = requests.get(baseURL)
     html = r.content.decode("utf-8")
     soup = BeautifulSoup(html, "lxml")
-    sel = soup.find("select")
-    for option in sel.find_all("option"):
-        if option.has_attr("value"):
-            areaId = option["value"]
-            areaName = option.text
-            area = {"id" : areaId, "name" : areaName}
-            allAreas.append(area)
-    allAreasJson = {"name" : "Areas", "data" : allAreas}
-    return allAreasJson
+    allAreas = json.loads(soup.find("script", attrs={"id": "menu-js"}).text)[0]["areas"]
+    return allAreas
 
 def getBuildings(university):
     allBuildings = []
@@ -28,154 +21,46 @@ def getBuildings(university):
     for area in areas:
         buildingsInArea = getBuildingsInArea(university, area["areaId"])
         allBuildings += buildingsInArea
-    buildingDict = {"buildings" : allBuildings}
-    return buildingDict
+    return allBuildings
 
 def getBuildingsInArea(university, areaId):
     buildings = []
     baseURL = getBaseURL(university)
-    url = baseURL + "?area=" + quote(areaId)
+    url = baseURL + "&area=" + quote(areaId)
     r = requests.get(url)
     html = r.content.decode("utf-8")
     soup = BeautifulSoup(html, "lxml")
-    buildingSelecter = soup.find("select", attrs={"name": "building"})
-    for option in buildingSelecter.find_all("option"):
-        if not option["value"] == "":
-            buildingId = option["value"]
-            buildingName = option.text
-            building = {"id" : buildingId, "name" : buildingName}
-            buildings.append(building)
-    buildingsInAreaJson = {"name" : "Buildings", "data" : buildings}
-    return buildingsInAreaJson
+    buildingsInArea = json.loads(soup.find("script", attrs={"id": "menu-js"}).text)[0]["buildings"]
+    return buildingsInArea
 
 def getRoomsInBuilding(university, areaId, buildingId):
     roomsInBuilding = []
     baseURL = getBaseURL(university)
-    url = baseURL + "?area=" + quote(areaId) + "&building=" + quote(buildingId)
+    url = baseURL + "&area=" + quote(areaId) + "&building=" + quote(buildingId)
     r = requests.get(url)
     html = r.content.decode("utf-8")
     soup = BeautifulSoup(html, "lxml")
-    roomSelecter = soup.find("select", attrs={"name": "id"})
-    for optGroup in roomSelecter.find_all("optgroup"):
-        typeId = optGroup["label"].strip()
-        for option in optGroup.find_all("option"):
-            if not option["value"] == "":
-                roomId = option["value"]
-                nameSize = re.split("(\(\d+\spl.\))", option.text)
-                roomName = nameSize[0].strip()
-                if len(nameSize)>1:
-                    size = re.split("(\d+)", nameSize[1])[1]
-                else:
-                    size = -1
-                room = {"id" : roomId, "name" : roomName, "typeid" : typeId, "size" : size}
-                roomsInBuilding.append(room)
-    roomsInBuildingJson = {"name" : "Rooms", "data" : roomsInBuilding}
-    return roomsInBuildingJson
-
-def getRoomData(university,areaId, buildingId, roomId):
-    baseURL = getBaseURL(university)
-    url = baseURL + "?area=" + quote(areaId) + "&building=" + quote(buildingId) + "&id=" + quote(roomId)
-    r = requests.get(url)
-    html = r.content.decode("utf-8")
-    soup = BeautifulSoup(html, "lxml")
-
-
-    someURL = soup.find("div", id="rominfo").find("a")
-    buildingName = soup.find("section", id="building").find("option", selected="selected").text.strip()
-
-    if someURL:
-        if buildingName == someURL.text.strip():
-            buildingURL = someURL["href"]
-            roomURL = "null"
-        else:
-            buildingURL = "null"
-            roomURL = someURL["href"]
-    else:
-        buildingURL = "none"
-        roomURL = "none"
-
-    buildingAcronym = buildingName[:4].strip() #Not ideal, but works for now
-
-    try:
-        imageURL = soup.find("div", id="rombilde").find("img")["src"].strip()
-    except:
-        imageURL = "null"
-
-    if imageURL == "":
-        imageURL = "null"
-
-    roomAcronym = soup.find("section", id="room").find("option", selected="selected").text.strip()[:4].strip()
-
-    data = {"buildingurl" : buildingURL, "buildingacronym" : buildingAcronym, "roomimg_url" : imageURL, "roomacronym" : roomAcronym, "roomurl" : roomURL}
-
-    equipmentList = []
-    try:
-        equipment = re.split(",", soup.find("div", id="equip").find("span", class_=None).text)
-    except:
-        equipment = ""
-    try:
-        av = soup.find("div", id="avinfo").find("span", class_=None).tex.strip()
-    except:
-        av = ""
-    try:
-        special = soup.find("div", id="specialities").find("span", class_=None).text.strip()
-    except:
-        special = ""
-
-    for eq in equipment:
-        equipmentList.append(eq.strip())
-    if not av == "":
-        equipmentList.append(av)
-    if not special == "":
-        equipmentList.append(special)
-
-    roomDataJson = {"name" : "Roomdata", "data" : data, "equipment" : equipmentList}
-
-    return roomDataJson
-
+    roomsInBuilding = json.loads(soup.find("script", attrs={"id": "menu-js"}).text)[0]["rooms"]
+    return roomsInBuilding
 
 def getWeekScheduleForRoom(university, areaId, buildingId, roomId, weeknumber, year):
     events = []
     timezone = strftime("%z", localtime())[:3]
     baseURL = getBaseURL(university)
-    url = baseURL + "?area=" + areaId + "&building=" + buildingId + "&id=" + roomId + "&week=" + str(weeknumber) + "&ar=" + str(year)
+    url = baseURL + "&area=" + areaId + "&building=" + buildingId + "&id=" + roomId + "&week=" + str(weeknumber) + "&ar=" + str(year)
     r = requests.get(url)
     html = r.text
     soup = BeautifulSoup(html, "lxml")
-    calendar = soup.find("div", class_="week-calendar")
-    for day in calendar.find_all("ul", class_="week-calendar__list--7"):
-        date = day.find("span", class_="is-visible--lg").text.strip().split(" ",1)[1].split("/", 1)
-        dateString = date[1] + "-" + date[0]
-        for eventSlot in day.find_all("li", class_="week-calendar__cell"):
-            if len(eventSlot.contents)>0:
-                weekday = getWeekday(eventSlot["id"])
-                content = eventSlot.find("div", class_="week-calendar__event__content")
-                contentList = content.find("a").text.split(":",1)
-                teachingMethodName = contentList[0]
-                summary = contentList[1]
-                timeTag = content.find("time")
-                if timeTag == None: #Some events has the time defined in a weird place...
-                    time = eventSlot.find("div", class_="modal__content ").find_all("span")[1].string
-                else:
-                    time = timeTag.text.strip()
-                
-                time = time.split("-",1)
-                timeStart = str(year) + "-" + dateString + "T" + time[0] + ":00" + timezone
-                timeEnd = str(year) + "-" + dateString + "T" + time[1] + ":00" + timezone
-                event = {"weekday" : weekday, "teaching-method-name": teachingMethodName, "summary": summary, "dtstart" : timeStart, "dtend" :timeEnd}
-                events.append(event)
-    eventsJson = {"name" : "Timetabledata", "events" : events}
-    return eventsJson
-
-def getWeekday(weekdayID):
-    wid = int(weekdayID[-1])-1
-    return ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"][wid]
+    events = json.loads(soup.find("script", attrs={"id": "data-js"}).text)
+    for event in events:
+        del event["room"]
+    return json.dumps(events)
 
 def getBaseURL(university):
     if university == "uio":
-        url = "https://tp.uio.no/timeplan/rom.php"
+        url = "https://tp.uio.no/timeplan/?type=room"
     else:
-        url = "https://tp.uio.no/" + university + "/timeplan/rom.php"
+        url = "https://tp.uio.no/" + university + "/timeplan/?type=room"
     return url
 
 
--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ import os
 from api import *
 from urllib.parse import unquote
 
-universities = ["uio", "uit", "ntnu"]
+universities = ["uib", "uio", "uit", "ntnu"]
 
 for uni in universities:
     filePrefix = "content/" + uni + "/"
@@ -12,28 +12,20 @@ for uni in universities:
             os.makedirs(filePrefix + "buildings")
     if not os.path.exists(filePrefix + "rooms"):
             os.makedirs(filePrefix + "rooms")
-    if not os.path.exists(filePrefix + "roomdata"):
-            os.makedirs(filePrefix + "roomdata")
 
     areaJson = getAreas(uni)
     with open(filePrefix + "areas.json", "w+") as areaFile:
         areaFile.write(json.dumps(areaJson))
 
-    for area in areaJson["data"]:
+    for area in areaJson:
         areaID = unquote(area["id"])
         buildingsInAreaJson = getBuildingsInArea(uni, areaID)
         with open(filePrefix + "buildings/" + areaID + ".json", "w+") as buildingsFile:
             buildingsFile.write(json.dumps(buildingsInAreaJson))
 
-        for building in buildingsInAreaJson["data"]:
+        for building in buildingsInAreaJson:
             buildingID = unquote(building["id"])
             roomsInBuildingJson = getRoomsInBuilding(uni, areaID, buildingID)
             with open(filePrefix + "rooms/" + areaID + buildingID + ".json", "w+") as roomsFile:
                 roomsFile.write(json.dumps(roomsInBuildingJson))
-
-            for room in roomsInBuildingJson["data"]:
-                roomID = unquote(room["id"])
-                roomDataJson = getRoomData(uni, areaID, buildingID, roomID)
-                with open(filePrefix + "roomdata/" + areaID + buildingID + roomID + ".json", "w+") as roomDataFile:
-                    roomDataFile.write(json.dumps(roomDataJson))
 
